e49e976a8bf729e1affb095f9d601e78d6dac7b1
Juan <juan.caraballo17@gmail.com>
2019-02-19 16:52:44 -0400

    Added initial django project files and configuration.

---
+++ src/logan_river/settings/development.py
@@ -0,0 +1,15 @@
+from logan_river.settings.base import *
+
+
+DEBUG = True
+
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = '/static/'
+
+SITE_URL = ''
---
+++ src/river_data/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RiverDataConfig(AppConfig):
+    name = 'river_data'
