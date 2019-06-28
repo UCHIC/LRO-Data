f5dcfbe837763585ab327ee86e9b2c9db810e897
Juan <juan.caraballo17@gmail.com>
2019-06-28 13:36:14 -0400

    Changed wsgi file to point to correct settings python module.

--- src/logan_river/wsgi.py
+++ src/logan_river/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logan_river.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logan_river.settings.linux_server')
 
 application = get_wsgi_application()
